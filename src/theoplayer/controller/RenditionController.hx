761b7d404a7bae4c2dc874efbd7e490c55c6e2e6
Andor Zöldesi <zandor86@gmail.com>
2016-07-21 15:47:24 +0200

    Comments added to RenditionController.hx

--- src/theoplayer/controller/RenditionController.hx
+++ src/theoplayer/controller/RenditionController.hx
@@ -5,7 +5,18 @@ package theoplayer.controller;
  * variable using the controller property using the renditionController name.
  */
 extern class RenditionController {
+
+  /**
+   * Gets a list of the available video renditions as rendition objects.
+   */
   public var renditions: Array<Redintion>;
 
+  /**
+   * Gets or sets the current playback rendition. Setting the playback 
+   * rendition locks the rendition to the given value. Setting the value to
+   * null will reactivate automatic quality selection. After setting the value,
+   * the player might continue playback in the current rendition until
+   * the playback buffer is depleted.
+   */
   public var activeRendition: Redintion;
 }
