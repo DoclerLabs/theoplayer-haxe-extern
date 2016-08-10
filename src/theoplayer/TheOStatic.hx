4807514f4d94a14e19ec7f1fc93c4ceec8ce2ed2
Andor Zöldesi <zandor86@gmail.com>
2016-08-10 13:18:05 +0200

    AR-183 neative name added for TheOStatic

--- src/theoplayer/TheOStatic.hx
+++ src/theoplayer/TheOStatic.hx
@@ -5,30 +5,31 @@ import haxe.extern.EitherType;
 /**
  * Class representing the global theoplayer variable.
  */
+@:native("theoplayer")
 extern class TheOStatic {
 
   /**
    * Sets the global player configuration before setup.
    */
-  public var configuration: Dynamic;
+  public static var configuration: Dynamic;
 
   /**
    * Function set in this property will be run after THEOplayer has been
    * loaded and auto-detected players have been created.
    */
-  public var onReady: Dynamic;
+  public static var onReady: Dynamic;
 
   /**
    * Returns the player linked to the given identifier. Identifier can be either
    * a sequence number, an HTML-id given to the <video>-tag to which the player
    * was attached or the player instance itself.
    */
-  public function player(identifier: Dynamic) : Player;
+  public static function player(identifier: Dynamic) : Player;
 
   /**
    * Returns the appropriate controller for the player identifier
    */
-  public function controller(identifier: Dynamic, name: Dynamic) : Dynamic;
+  public static function controller(identifier: Dynamic, name: Dynamic) : Dynamic;
 
   /**
    * 	Destroys the player linked to the given identifier. This removes the
@@ -36,15 +37,15 @@ extern class TheOStatic {
    * The destroyed player can no longer be used: any attempt to access
    * properties or methods of a destroyed player results in undefined behavior.
    */
-  public function destroy(identifier: Dynamic) : Void;
+  public static function destroy(identifier: Dynamic) : Void;
 
   /**
    * Adds the given event listener for the given global event.
    */
-  public function addEventListener(eventName: String, listener: Dynamic) : Void;
+  public static function addEventListener(eventName: String, listener: Dynamic) : Void;
 
   /**
    * Removes the given event listener for the given global event.
    */
-  public function removeEventListener(eventName: String, listener: Dynamic) : Void;
+  public static function removeEventListener(eventName: String, listener: Dynamic) : Void;
 }
